844ad5ac35f33eafcc3a5aa25e6d2b61b1ac6884
N-Nagorny <Nikiton-N@Mail.Ru>
2019-03-15 13:54:27 +0300

    Update prepare_images.sh

--- prepare_images.sh
+++ prepare_images.sh
@@ -23,7 +23,7 @@ done
 
 unzip BOSSbase_1.01.zip
 mkdir -p BOSSbase_1.01-$SIZE/cover
-mkdir -p BOSSbase_1.01-$SIZE/stego
+mkdir -p BOSSbase_1.01-$SIZE/stego_wow_0.4
 
 if [ $SIZE == 512 ]; then
     mv BOSSbase_1.01/* BOSSbase_1.01-$SIZE/cover
@@ -42,12 +42,12 @@ do
     DIRNAME=$(( I/1000 ))
     if [ $(( I%1000 )) == 0 ]; then
         mkdir -p BOSSbase_1.01-$SIZE/cover/$DIRNAME
-        mkdir -p BOSSbase_1.01-$SIZE/stego/$DIRNAME
+        mkdir -p BOSSbase_1.01-$SIZE/stego_wow_0.4/$DIRNAME
     fi
     mv BOSSbase_1.01-$SIZE/cover/$(( I+1 )).pgm BOSSbase_1.01-$SIZE/cover/$DIRNAME/$(( I+1 )).pgm
 done
 
 for I in {0..9}
 do
-    ./WOW_linux_make_v10/executable/WOW -v -I BOSSbase_1.01-$SIZE/cover/$I/ -O BOSSbase_1.01-$SIZE/stego/$I/ -a 0.4
+    ./WOW_linux_make_v10/executable/WOW -v -I BOSSbase_1.01-$SIZE/cover/$I/ -O BOSSbase_1.01-$SIZE/stego_wow_0.4/$I/ -a 0.4
 done
